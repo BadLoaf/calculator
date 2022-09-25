783fca43ff7df6de1fabb138e19014df8efbe99d
BadLoaf <aj.sth.2020@gmail.com>
2022-09-25 18:04:38 +0545

    added keyboard support for numbers

--- src/Components/Calculator.js
+++ src/Components/Calculator.js
@@ -1,37 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Calculator.css';
 
 function Calculator() {
-    let [display, setDisplay] = useState('')
-    let [toClear, setToClear] = useState(false)
+    let [display, setDisplay] = useState('');
+    let [toClear, setToClear] = useState(false);
+
+    useEffect(() => {
+        document.addEventListener('keydown', detectKeyDown, true)
+    }, [])
+
+    function detectKeyDown(e) {
+        if (/\d|\./.test(e.key)) {
+            updateCalc(e.key);
+        }
+    }
 
     function clearDisplay() {
-        setDisplay("")
+        setDisplay("");
     }
 
     function evaluate() {
         setToClear(true);
         setDisplay(
             eval(display)
-        )
+        );
     }
     
     function backspace() {
         setDisplay(
             display.slice(0,-1)
-        )
+        );
     }
 
     function setNegative() {
         if (display === '') {
-            setDisplay(
-                '-'
-            )
+            setDisplay('-');
         }
     }
 
     function updateCalc(value) {
-        const ops = ['+', '-', '*', '/']
+        const ops = ['+', '-', '*', '/', '.']
         if (
             (ops.includes(value) && ops.includes(display.slice(-1)) )
             ||
@@ -56,25 +64,25 @@ function Calculator() {
             <div id="centerer">
                 <div id='calc'>
                     <div id="display">{display}</div>
-                    <button id="clear" class="modifiers"onClick={clearDisplay}>C</button>
-                    <button id="negative" class="modifiers"onClick={setNegative}>+/-</button>
-                    <button id="back" class="modifiers"onClick={backspace}>B</button>
-                    <button id="/" class="operator"onClick={() => updateCalc('/')}>/</button>
-                    <button id="7" onClick={() => updateCalc('7')}>7</button>
-                    <button id="8" onClick={() => updateCalc('8')}>8</button>
-                    <button id="9" onClick={() => updateCalc('9')}>9</button>
-                    <button id="x" class="operator"onClick={() => updateCalc('*')}>x</button>
-                    <button id="4" onClick={() => updateCalc('4')}>4</button>
-                    <button id="5" onClick={() => updateCalc('5')}>5</button>
-                    <button id="6" onClick={() => updateCalc('6')}>6</button>
-                    <button id="-" class="operator"onClick={() => updateCalc('-')}>-</button>
-                    <button id="1" onClick={() => updateCalc('1')}>1</button>
-                    <button id="2" onClick={() => updateCalc('2')}>2</button>
-                    <button id="3" onClick={() => updateCalc('3')}>3</button>
-                    <button id="+" class="operator"onClick={() => updateCalc('+')}>+</button>
-                    <button id="zero" onClick={() => updateCalc('0')}>0</button>
+                    <button id="clear" className="modifiers"onClick={clearDisplay}>C</button>
+                    <button id="negative" className="modifiers"onClick={setNegative}>+/-</button>
+                    <button id="back" className="modifiers"onClick={backspace}>B</button>
+                    <button id="/" className="operator" onClick={() => updateCalc('/')}>/</button>
+                    <button id="7" className="digits" onClick={() => updateCalc('7')}>7</button>
+                    <button id="8" className="digits" onClick={() => updateCalc('8')}>8</button>
+                    <button id="9" className="digits" onClick={() => updateCalc('9')}>9</button>
+                    <button id="x" className="operator" onClick={() => updateCalc('*')}>x</button>
+                    <button id="4" className="digits" onClick={() => updateCalc('4')}>4</button>
+                    <button id="5" className="digits" onClick={() => updateCalc('5')}>5</button>
+                    <button id="6" className="digits" onClick={() => updateCalc('6')}>6</button>
+                    <button id="-" className="operator" onClick={() => updateCalc('-')}>-</button>
+                    <button id="1" className="digits" onClick={() => updateCalc('1')}>1</button>
+                    <button id="2" className="digits" onClick={() => updateCalc('2')}>2</button>
+                    <button id="3" className="digits" onClick={() => updateCalc('3')}>3</button>
+                    <button id="+" className="operator" onClick={() => updateCalc('+')}>+</button>
+                    <button id="zero" className="digits" onClick={() => updateCalc('0')}>0</button>
                     <button id="dot" onClick={() => updateCalc('.')}>.</button>
-                    <button id="=" class="operator"onClick={evaluate}>=</button>
+                    <button id="=" className="operator"onClick={evaluate}>=</button>
                 </div>
             </div>
         )
